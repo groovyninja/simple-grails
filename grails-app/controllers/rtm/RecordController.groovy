3a8d2b44f617762acb7a9f8ec634a070fa0977de
groovyninja <roman.gordeev@mail.ru>
2014-08-07 14:12:15 +0400

    grails create-domain-class rtm.Record&& grails create-controller rtm.Record

---
+++ grails-app/controllers/rtm/RecordController.groovy
@@ -0,0 +1,11 @@
+package rtm
+
+/**
+ * Controller related to {@link rtm.Record} model class.
+ * We are using scaffolding here to generate views and 
+ * controller actions at run time.
+ */
+class RecordController {
+    static scaffold = rtm.Record // <-- magic starts here
+    //def index() { }
+}
